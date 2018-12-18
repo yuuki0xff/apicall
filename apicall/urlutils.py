ec72b1b451fcd52cff1711d544773fa1a1e505aa
yuuki <yuuki0xff@gmail.com>
2018-12-18 16:41:02 +0900

    fix bug that missing query if url is absolute

--- apicall/urlutils.py
+++ apicall/urlutils.py
@@ -2,18 +2,41 @@ import typing
 import urllib.parse
 
 
+def is_absolute_url(url) -> bool:
+    u = urllib.parse.urlsplit(url, scheme='http')
+    return u.netloc != ''
+
+
+def escape_query(query: str) -> str:
+    name, value = query.split('=', maxsplit=1)
+    return urllib.parse.quote(name) + '=' + urllib.parse.quote(value)
+
+
 def concat_urls(
         endpoints: typing.Tuple[str, ...],
         url: str,
         queries: typing.Tuple[str, ...],
 ) -> typing.Tuple[str, ...]:
     u = urllib.parse.urlsplit(url, scheme='http')
-    url_is_abs = u.netloc != ''
-    if url_is_abs:
-        return (urllib.parse.urlunsplit(u), )
+    escaped_queries = list(escape_query(q) for q in queries)
+    if u.query:
+        escaped_queries.insert(0, u.query)
+    query_string = tuple('&'.join(escaped_queries))
 
-    escaped_queries = tuple(urllib.parse.quote(q) for q in queries)
-    query_string = '&'.join((u.query, ) + escaped_queries)
+    if is_absolute_url(url):
+        components = (
+            # scheme
+            u.scheme,
+            # hostname
+            u.netloc or 'localhost',
+            # path
+            u.path.lstrip('/'),
+            # query
+            query_string,
+            # fragment
+            '',
+        )
+        return urllib.parse.urlunsplit(components),
 
     concated_urls = []
     for endpoint in endpoints:
