e6b6052ddd91edab8250edf68ded22b169668893
yuuki <yuuki0xff@gmail.com>
2018-12-18 17:17:50 +0900

    add test cases

--- tests/test_urlutils.py
+++ tests/test_urlutils.py
@@ -1,4 +1,26 @@
-from apicall.urlutils import concat_urls
+from apicall.urlutils import is_absolute_url, escape_query, concat_urls
+
+
+class TestIsAbsoluteUrl:
+    def test_missing_schema(self):
+        assert is_absolute_url('//localhost/path')
+
+    def test_missing_hostname(self):
+        assert not is_absolute_url('/path-only')
+
+    def test_relative_path(self):
+        assert not is_absolute_url('get')
+
+
+class TestEscapeQuery:
+    def test_normal(self):
+        assert escape_query('name=value') == 'name=value'
+
+    def test_escape_key(self):
+        assert escape_query('a b=value') == 'a%20b=value'
+
+    def test_escape_value(self):
+        assert escape_query('name=a b') == 'name=a%20b'
 
 
 class TestConcatUrls:
